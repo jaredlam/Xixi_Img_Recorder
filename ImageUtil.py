f7c456adb17d6ce35b1a4e26c7fc76f2969f4e6d
LuoMingYang <luomingyang@camera360.com>
2017-06-13 14:51:29 +0800

    [罗鸣阳] support Chinese

--- ImageUtil.py
+++ ImageUtil.py
@@ -1,4 +1,4 @@
-import PIL
+# coding=utf-8
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
@@ -8,9 +8,9 @@ def set_text(imagefile, text):
     img = Image.open(imagefile)
     (img_x, img_y) = img.size
 
-    # ttfont = ImageFont.truetype('/usr/share/fonts/noto/NotoSansCJK-Regular.ttc', int(img_y / 20))
+    ttfont = ImageFont.truetype('msyh.ttc', int(img_y / 20))
 
     draw = ImageDraw.Draw(img)
-    draw.text((int(img_x / 20), img_y - int((img_y * 1.3) / 20)), text, (0, 0, 0))
+    draw.text((int(img_x / 20), img_y - int((img_y * 3) / 20)), unicode(text, "UTF-8"), (74, 74, 170), font=ttfont)
 
     img.save('target.jpg', 'jpeg')
--- ImgRecorder.py
+++ ImgRecorder.py
@@ -2,11 +2,13 @@ import exifread
 import DateUtil
 import ImageUtil
 
-f = open("IMG_20170530_132435.jpg", 'rb')
+source_img = "IMG_20170407_221436.jpg"
+
+f = open(source_img, 'rb')
 tags = exifread.process_file(f)
 
 for tag in tags.keys():
     # print "key:%s, value:%s" % (tag, tags[tag])
     if tag == "Image DateTime":
         diff_str = DateUtil.get_days(tags[tag])
-        ImageUtil.set_text("IMG_20170530_132435.jpg", diff_str)
+        ImageUtil.set_text(source_img, diff_str)
